11fc06ea0f30f34d545075589747b712e9fe92d8
Abdulrahman <am.amoudi@outlook.com>
2020-08-11 00:00:29 +0300

    init project

---
+++ Multiple Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Storyboard
+//
+//  Created by Abdulrahman Al-amoudi on 10/08/2020.
+//  Copyright © 2020 Abdulrahman Al-amoudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
